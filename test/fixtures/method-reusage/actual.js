ed56d29a29b445481bb416b162cd5f8a719d50ef
Graeme Yeates <yeatesgraeme@gmail.com>
2015-08-05 16:10:21 -0400

    Ensure a method is only ever imported once

---
+++ test/fixtures/method-reusage/actual.js
@@ -0,0 +1,6 @@
+import {map} from 'lodash';
+
+let result = map([1, 2, 3], function() {});
+let result2 = map([], function() {});
+let result3 = map([], function() {});
+let result4 = map([], function() {});
