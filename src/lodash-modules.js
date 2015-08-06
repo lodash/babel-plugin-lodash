11af03bc0f9ddf727b675ea39762ee7d5ff3073b
John-David Dalton <john.david.dalton@gmail.com>
2015-08-06 15:10:10 -0700

    Cleanup modules & drop lodash-cli dep

--- src/lodash-modules.js
+++ src/lodash-modules.js
@@ -1,35 +1,28 @@
 import path from 'path';
-import {includes, result, zipObject} from 'lodash';
+import {findKey, transform} from 'lodash';
 import fs from 'fs';
-import {aliasToReal} from 'lodash-cli/lib/mapping';
 
 // Slow synchronous version of https://github.com/megawac/lodash-modularize/blob/master/src/lodashModules.js
 // Using the paths lodash-cli provides is not an option as they may change version to version =(
 
-function getDirectories(srcpath) {
-  return fs.readdirSync(srcpath).filter(file =>
-        fs.statSync(path.join(srcpath, file)).isDirectory());
+function getDirectories(srcPath) {
+  return fs.readdirSync(srcPath).filter(filePath =>
+    fs.statSync(path.join(srcPath, filePath)).isDirectory());
 }
 
 const expectedPath = './node_modules/lodash';
 const modularizePath = path.join(__dirname, '../node_modules/lodash');
 
 let lodashPath = fs.existsSync(expectedPath) ? expectedPath : modularizePath;
-let m = getDirectories(lodashPath)
 
-let funcs = m.map(val => {
-  return fs.readdirSync(path.join(lodashPath, val)).map(name => name.slice(0, -3));
-});
-
-export let modules = zipObject(m, funcs)
+let categoryMap = transform(getDirectories(lodashPath), (result, category) => {
+  result[category] = fs.readdirSync(path.join(lodashPath, category)).map(name => path.basename(name, '.js'));
+}, {});
 
 export default function resolveModule(name) {
-  let realName = result(aliasToReal, name, name);
-
-  for (var category in modules) {
-    if (includes(modules[category], realName)) {
-      return path.join('lodash', category, realName);
-    }
+  let category = findKey(categoryMap, funcs => funcs.indexOf(name) > -1);
+  if (category) {
+    return path.join('lodash', category, name);
   }
   throw new Error(`lodash method ${name} was not in known modules
     Please file a bug if it's my fault https://github.com/megawac/babel-plugin-lodash/issues
