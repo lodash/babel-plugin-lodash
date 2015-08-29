4be5d930540460d6eb98fbe26f95f7b940dbb84a
Graeme Yeates <yeatesgraeme@gmail.com>
2015-08-29 08:48:05 -0400

    Coverage for explict lodash-fp/convert use

---
+++ test/fixtures/lodash-fp-explicit-manual-convert/actual.js
@@ -0,0 +1,6 @@
+import {map, filter} from 'lodash';
+import convert from 'lodash-fp/convert';
+
+const fp = convert({
+  map, filter
+});
---
+++ test/fixtures/lodash-fp-explicit-manual-convert/expected.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _lodashFpConvert = require('lodash-fp/convert');
+
+var _lodashFpConvert2 = _interopRequireDefault(_lodashFpConvert);
+
+var fp = (0, _lodashFpConvert2['default'])({
+  map: map, filter: filter
+});
