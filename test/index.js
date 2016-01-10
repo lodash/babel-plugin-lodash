2a9e2c214b4b1462fd93175fe0542825e6a9e0e8
John-David Dalton <john.david.dalton@gmail.com>
2016-01-09 16:58:27 -0800

    Use single quotes and style nits.

--- test/index.js
+++ test/index.js
@@ -1,36 +1,36 @@
-import path from "path";
-import fs from "fs";
-import assert from "assert";
-import {transformFileSync} from "babel-core";
-import plugin from "../src/index";
-import {trim} from "lodash";
+import path from 'path';
+import fs from 'fs';
+import assert from 'assert';
+import { transformFileSync } from 'babel-core';
+import plugin from '../src/index';
+import { trim } from 'lodash';
 
-describe("Lodash modularized builds", () => {
-  const fixturesDir = path.join(__dirname, "fixtures");
-  const errorFixturesDir = path.join(__dirname, "error-fixtures");
+describe('Lodash modularized builds', () => {
+  const fixturesDir = path.join(__dirname, 'fixtures');
+  const errorFixturesDir = path.join(__dirname, 'error-fixtures');
 
   fs.readdirSync(fixturesDir).map(caseName => {
     const fixtureDir = path.join(fixturesDir, caseName);
-    const actualFile = path.join(fixtureDir, "actual.js");
-    const expectedFile = path.join(fixtureDir, "expected.js");
+    const actualFile = path.join(fixtureDir, 'actual.js');
+    const expectedFile = path.join(fixtureDir, 'expected.js');
 
-    it(`should work with ${caseName.split("-").join(" ")}`, () => {
+    it(`should work with ${caseName.split('-').join(' ')}`, () => {
       const actual = transformFileSync(actualFile, {
-        plugins: [plugin]
+        'plugins': [plugin]
       }).code;
-      const expected = fs.readFileSync(expectedFile).toString();
+      const expected = fs.readFileSync(expectedFile, 'utf8');
       assert.equal(trim(actual), trim(expected));
     });
   });
 
   fs.readdirSync(errorFixturesDir).map(caseName => {
     const fixtureDir = path.join(errorFixturesDir, caseName);
-    const actualFile = path.join(fixtureDir, "actual.js");
+    const actualFile = path.join(fixtureDir, 'actual.js');
 
-    it(`should throw an error with ${caseName.split("-").join(" ")}`, () => {
+    it(`should throw an error with ${caseName.split('-').join(' ')}`, () => {
       assert.throws(function() {
         const actual = transformFileSync(actualFile, {
-          plugins: [plugin]
+          'plugins': [plugin]
         }).code;
       });
     });
