8cb8945b9677b9d560be5f6841573fcccf9231ee
Graeme Yeates <yeatesgraeme@gmail.com>
2016-01-15 14:25:12 -0500

    Closes #26; can't reproduce

---
+++ test/fixtures/issue-26/actual.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import { join } from 'lodash';
+
+let result = _.join(['a', 'b', 'c'], '~');
+
+result = join(['a', 'b', 'c'], '~');
---
+++ test/fixtures/issue-26/expected.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var _join = require('lodash/join');
+
+var _join2 = _interopRequireDefault(_join);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var result = (0, _join2.default)(['a', 'b', 'c'], '~');
+
+result = (0, _join2.default)(['a', 'b', 'c'], '~');
