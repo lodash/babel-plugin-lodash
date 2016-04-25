2daf92d7c36a284827d47ade5da2583ef28d064d
John-David Dalton <john.david.dalton@gmail.com>
2016-04-24 19:25:40 -0700

    Cleanup lodash-modules.

--- src/lodash-modules.js
+++ src/lodash-modules.js
@@ -1,40 +1,35 @@
 'use strict';
 
+import _ from 'lodash';
 import fs from 'fs';
-import { assign, findKey, transform } from 'lodash';
 import Module from 'module';
 import path from 'path';
 
 function getDirectories(srcPath) {
-  // Slow synchronous version of https://github.com/megawac/lodash-modularize/blob/master/src/lodashModules.js.
-  // Using the paths lodash-cli provides is not an option as they may change version to version =(
   return ['.'].concat(fs.readdirSync(srcPath)).filter(filePath =>
     fs.statSync(path.join(srcPath, filePath)).isDirectory());
 }
 
-const lodashPath = path.dirname(Module._resolveFilename('lodash', assign(new Module, {
+const lodashPath = path.dirname(Module._resolveFilename('lodash', _.assign(new Module, {
   'paths': Module._nodeModulePaths(process.cwd())
 })));
 
-const categoryMap = transform(getDirectories(lodashPath), (result, category) => {
+const categoryMap = _.transform(getDirectories(lodashPath), (result, category) => {
   result[category] = fs.readdirSync(path.join(lodashPath, category))
     .filter(name => path.extname(name) == '.js')
     .map(name => path.basename(name, '.js'));
 }, {});
 
 export default function resolveModule(name, base) {
-  let category;
+  const category = base
+    ? (_.includes(categoryMap[base], name) && base)
+    : _.findKey(categoryMap, funcs => _.includes(funcs, name));
 
-  if (base) {
-    category = categoryMap[base].indexOf(name) > -1 && base;
-  } else {
-    category = findKey(categoryMap, funcs => funcs.indexOf(name) > -1);    
+  if (!category) {
+    throw new Error([
+      `Lodash method ${name} is not a known module.`,
+      'Please report bugs to https://github.com/lodash/babel-plugin-lodash/issues.'
+    ].join('\n'));
   }
-  if (category) {
-    return path.join('lodash', category, name);
-  }
-  throw new Error([
-    `lodash method ${name} was not in known modules.`,
-    'Please report bugs to https://github.com/lodash/babel-plugin-lodash/issues.'
-  ].join('\n'));
+  return path.join('lodash', category, name);
 }
