3a4fae6ed3d592f9be4d936a1c41a31463d57a5d
John-David Dalton <john.david.dalton@gmail.com>
2016-04-27 00:40:58 -0700

    Update expected spread result.

--- test/skipped-fixtures/lodash-spread-operator/expected.js
+++ test/skipped-fixtures/lodash-spread-operator/expected.js
@@ -12,5 +12,5 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-_merge6.default.apply(_merge6, [{}].concat(_toConsumableArray(args)))
-_merge4.default.apply(_merge4, [{}].concat(_toConsumableArray(args)));
+_merge6.default.apply(undefined, [{}].concat(_toConsumableArray(args)))
+_merge4.default.apply(undefined, [{}].concat(_toConsumableArray(args)));
