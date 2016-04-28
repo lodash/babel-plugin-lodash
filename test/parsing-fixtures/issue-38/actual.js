25aa06b81023728236e52ffa5eec195571ad5c04
John-David Dalton <john.david.dalton@gmail.com>
2016-04-27 21:20:22 -0700

    Simplify parsing test.

--- test/parsing-fixtures/issue-38/actual.js
+++
@@ -1,9 +0,0 @@
-// Various variable declarators and property patterns which can apparently break
-
-for (const a of b) {}
-var foo, bar;
-let x, y;
-const d = 1, e = 2;
-export default {
-  [process.env.NODE_ENV]: process.env.NODE_ENV
-};
