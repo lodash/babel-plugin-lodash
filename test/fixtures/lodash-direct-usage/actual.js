d3d3c2193d30dace02e5d8ed5555a9b59acea60d
John-David Dalton <john.david.dalton@gmail.com>
2016-06-11 13:38:05 -0700

    Cleanup direct-usage tests.

--- test/fixtures/lodash-direct-usage/actual.js
+++ test/fixtures/lodash-direct-usage/actual.js
@@ -1,3 +1,4 @@
 import _ from 'lodash';
 
-const result = _.map([1, 2, 3], String);
+const result = _.map([-1, 0, 1], n => _.add(1, n));
+_.take(_.reject(result, Boolean), 1);
--- test/fixtures/lodash-multi-direct/actual.js
+++
@@ -1,4 +0,0 @@
-import _ from 'lodash';
-
-const result = _.map([1, 2, 3], function() {});
-_.take(_.reject(result), 1);
