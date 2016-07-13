2f16b4ead37f9b780237e21ab755add25896a2b1
John-David Dalton <john.david.dalton@gmail.com>
2016-07-12 18:18:54 -0700

    Split `ModuleCache` out of `config`.

---
+++ src/ModuleCache.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+import fs from 'fs';
+import glob from 'glob';
+import MapCache from './MapCache';
+import Module from 'module';
+import path from 'path';
+
+/*----------------------------------------------------------------------------*/
+
+export default class ModuleCache extends MapCache {
+  constructor(moduleRoot='') {
+    super();
+
+    const dirPaths = moduleRoot ? glob.sync(path.join(moduleRoot, '**/')) : [];
+    _.each(dirPaths, dirPath => {
+      const base = path.relative(moduleRoot, dirPath);
+      const filePaths = glob.sync(path.join(dirPath, '*.js'));
+      const pairs = _.map(filePaths, filePath => {
+        const name = path.basename(filePath, '.js');
+        return [name.toLowerCase(), name];
+      });
+      this.set(base, new MapCache(pairs));
+    });
+  }
+
+  static resolve(id, from=process.cwd()) {
+    try {
+      const dirs = path.dirname(Module._resolveFilename(id, _.assign(new Module, {
+        'paths': Module._nodeModulePaths(from)
+      }))).split(path.sep);
+
+      let { length } = dirs;
+      while (length--) {
+        const dirSub  = dirs.slice(0, length + 1);
+        const dirPath = dirSub.join('/');
+        const pkgPath = path.join(dirPath, 'package.json');
+
+        if ((length && dirs[length - 1] == 'node_modules') ||
+            (fs.existsSync(pkgPath) && require(pkgPath).name == id)) {
+          return dirPath;
+        }
+      }
+      return dirs.join('/');
+    } catch (e) {}
+    return '';
+  }
+};
--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
 import _ from 'lodash';
-import fs from 'fs';
-import glob from 'glob';
 import MapCache from './MapCache';
-import Module from 'module';
-import path from 'path';
+import ModuleCache from './ModuleCache';
 
 const defaultIds = [
   'lodash',
@@ -14,61 +11,21 @@ const defaultIds = [
 const ids = [];
 
 const modules = _.transform(defaultIds, (modules, id) => {
-  const moduleRoot = getModuleRoot(id);
+  const moduleRoot = ModuleCache.resolve(id);
   if (moduleRoot) {
     ids.push(id);
-    modules.set(id, createModuleMap(moduleRoot));
+    modules.set(id, new ModuleCache(moduleRoot));
   }
 }, new MapCache);
 
 /*----------------------------------------------------------------------------*/
 
-function createModuleMap(moduleRoot) {
-  const basePaths = moduleRoot ? glob.sync(path.join(moduleRoot, '**/')) : [];
-  return _.reduce(basePaths, (result, basePath) => {
-    const base = path.relative(moduleRoot, basePath);
-    const filePaths = glob.sync(path.join(basePath, '*.js'));
-    const pairs = filePaths.map(filePath => {
-      const name = path.basename(filePath, '.js');
-      return [name.toLowerCase(), name];
-    });
-    return result.set(base, new MapCache(pairs));
-  }, new MapCache);
-}
-
-function getModuleRoot(id, from=process.cwd()) {
-  try {
-    const dirs = path.dirname(Module._resolveFilename(id, _.assign(new Module, {
-      'paths': Module._nodeModulePaths(from)
-    }))).split(path.sep);
-
-    if (dirs[0] == '') {
-      dirs[0] = path.sep;
-    }
-    let { length:index } = dirs;
-    while (index--) {
-      const dirSub  = dirs.slice(0, index + 1);
-      const dirPath = dirSub.join('/');
-      const pkgPath = path.join(dirPath, 'package.json');
-
-      if (dirs[index - 1] === 'node_modules' ||
-          (fs.existsSync(pkgPath) && require(pkgPath).name == id)) {
-        return dirPath;
-      }
-    }
-    return dirs.join('/');
-  } catch (e) {}
-  return '';
-}
-
-function config({ cwd=process.cwd(), id=ids[0] } = {}) {
+export default function config({ cwd=process.cwd(), id=ids[0] }={}) {
   _.each(_.castArray(id), id => {
     if (!modules.get(id)) {
       ids.push(id);
-      modules.set(id, createModuleMap(getModuleRoot(id, cwd)));
+      modules.set(id, new ModuleCache(ModuleCache.resolve(id, cwd)));
     }
   });
   return { ids, modules };
-}
-
-export default config;
+};
